1a0f9038af96feca7ff7e0578becc0e6db9a5fee
Marvin <marvin.therolf@gmail.com>
2021-06-06 15:33:36 +0200

    Add test cases

--- EineListe/src/test/java/ListeTest.java
+++ EineListe/src/test/java/ListeTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class ListeTest {
 
@@ -9,10 +10,12 @@ class ListeTest {
 
     @BeforeEach
     void aufsetzen() {
-        liste = new KistenListe("kistenListe");
+        liste = new KistenListe("Kisten Liste");
+        // liste = new ArrayListe("Array Liste");
     }
 
-    @Test void anschauen() {
+    @Test
+    void anhaengenUndAnschauen() {
         // Gegeben
         liste.anhaengen("moin");
 
@@ -23,25 +26,223 @@ class ListeTest {
         assertThat(ergebnis).isEqualTo("moin");
     }
 
-    @Test void anhaengen() {
+    @Test
+    void anhaengenUndAnschauen_mehrereElemente() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
 
+        // Wenn
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+        String ergebnis3 = liste.anschauen(2);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("moin");
+        assertThat(ergebnis2).isEqualTo("was");
+        assertThat(ergebnis3).isEqualTo("geht");
     }
 
-    @Test void aussortieren() {
+    @Test
+    void anhaengenUndAnschauen_Null() {
+        // Gegeben
+        liste.anhaengen(null);
 
+        // Wenn
+        String ergebnis = liste.anschauen(0);
+
+        // Dann
+        assertThat(ergebnis).isEqualTo(null);
     }
 
-    @Test void einsortieren() {
-
+    @Test
+    void anschauen_IndexZuGross() {
+        // Wenn, Dann
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> liste.anschauen(1));
     }
 
-    @Test void ersetzen() {
-
+    @Test
+    void anschauen_IndexZuKlein() {
+        // Wenn, Dann
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> liste.anschauen(-1));
     }
 
-    @Test void machLeer() {
+    @Test
+    void aussortieren() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
 
+        // Wenn
+        liste.aussortieren("was");
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("moin");
+        assertThat(ergebnis2).isEqualTo("geht");
+    }
+
+    @Test
+    void aussortieren_elementGibtEsNicht() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
+
+        // Wenn
+        liste.aussortieren("nicht");
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+        String ergebnis3 = liste.anschauen(2);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("moin");
+        assertThat(ergebnis2).isEqualTo("was");
+        assertThat(ergebnis3).isEqualTo("geht");
     }
 
 
+    @Test
+    void aussortieren_null() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
+
+        // Wenn
+        liste.aussortieren("nicht");
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+        String ergebnis3 = liste.anschauen(2);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("moin");
+        assertThat(ergebnis2).isEqualTo("was");
+        assertThat(ergebnis3).isEqualTo("geht");
+    }
+
+    @Test
+    void einsortieren() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
+
+        // Wenn
+        liste.einsortieren("doch", 1);
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+        String ergebnis3 = liste.anschauen(2);
+        String ergebnis4 = liste.anschauen(3);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("moin");
+        assertThat(ergebnis2).isEqualTo("doch");
+        assertThat(ergebnis3).isEqualTo("was");
+        assertThat(ergebnis4).isEqualTo("geht");
+    }
+
+    @Test
+    void einsortieren_vorne() {
+        // Wenn
+        liste.einsortieren("geht", 0);
+        liste.einsortieren("was", 0);
+        liste.einsortieren("moin", 0);
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+        String ergebnis3 = liste.anschauen(2);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("moin");
+        assertThat(ergebnis2).isEqualTo("was");
+        assertThat(ergebnis3).isEqualTo("geht");
+    }
+
+    @Test
+    void einsortieren_null() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
+
+        // Wenn
+        liste.einsortieren(null, 1);
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+        String ergebnis3 = liste.anschauen(2);
+        String ergebnis4 = liste.anschauen(3);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("moin");
+        assertThat(ergebnis2).isEqualTo(null);
+        assertThat(ergebnis3).isEqualTo("was");
+        assertThat(ergebnis4).isEqualTo("geht");
+    }
+
+    @Test
+    void einsortieren_indexZuGross() {
+        // Wenn, Dann
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> liste.einsortieren("moin", 1));
+    }
+
+    @Test
+    void einsortieren_indexZuKlein() {
+        // Wenn, Dann
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> liste.einsortieren("moin", -1));
+    }
+
+    @Test
+    void ersetzen() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
+
+        // Wenn
+        liste.ersetzen("servus", 0);
+        String ergebnis1 = liste.anschauen(0);
+        String ergebnis2 = liste.anschauen(1);
+        String ergebnis3 = liste.anschauen(2);
+
+        // Dann
+        assertThat(ergebnis1).isEqualTo("servus");
+        assertThat(ergebnis2).isEqualTo("was");
+        assertThat(ergebnis3).isEqualTo("geht");
+    }
+
+    @Test
+    void ersetzen_indexZuGross() {
+        // Wenn, Dann
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> liste.ersetzen("servus", 0));
+    }
+
+    @Test
+    void ersetzen_indexZuKlein() {
+        // Wenn, Dann
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> liste.ersetzen("servus", -1));
+    }
+
+    @Test
+    void machLeer() {
+        // Gegeben
+        liste.anhaengen("moin");
+        liste.anhaengen("was");
+        liste.anhaengen("geht");
+
+        // Wenn, Dann
+        liste.machLeer();
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> liste.anschauen(0));
+    }
 }
+
+// Liste sollte eine Methode laenge() haben
